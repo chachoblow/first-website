892812fb123fdb40fb7bcdfa4369158dcd297ee4
chachoblow <wesleyr.klein@gmail.com>
2019-02-23 17:02:55 -0800

    Redisgned homepage.

---
+++ js/menu-images-fade.js
@@ -0,0 +1,23 @@
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var slides = document.getElementsByClassName("nav-image");
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+
+  
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
